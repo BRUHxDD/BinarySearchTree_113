894270f5f9d0c0f1489f517b686ae3e620437218
Teuku Muhammad Attar Fadhilah <teuku.muhammad.ft21@mail.umy.ac.id>
2022-12-26 17:28:31 +0700

    add public void insert

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -29,6 +29,33 @@ namespace BinarySearchTree
         {
             ROOT = null; // initializing root to null
         }
+        public void insert(string element)// insert a node in the binary //
+        {
+            node tmp, parent = null, currentnode = null;
+            find(element, ref parent, ref currentnode);
+            if (currentnode!= null)
+            {
+                Console.WriteLine("Duplicate words not allowed");
+                return;
+            }
+            else //if the speicified node is not present
+            {
+                tmp = new node(element, null, null);
+                if(parent == null)
+                {
+                    ROOT = tmp;
+                }
+                else if (string.Compare(element,parent.info) < 0)
+                {
+                    if (string.Compare(element, parent.info) <0)
+                        parent.lchild = tmp;
+                }
+                else
+                {
+                    parent.rchild = tmp;
+                }
+            }
+        }
     }
     internal class Program
     {
