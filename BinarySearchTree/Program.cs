d08ff8b77d169161f4a26f3690298b99d7d3f7b2
Teuku Muhammad Attar Fadhilah <teuku.muhammad.ft21@mail.umy.ac.id>
2022-12-26 17:51:21 +0700

    add case 4

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -142,6 +142,11 @@ namespace BinarySearchTree
                         break;
                     case '3':
                         {
+                            x.preorder(x.ROOT);
+                        }
+                        break;
+                    case '4':
+                        {
                             x.postorder(x.ROOT);
                         }
                         break;
