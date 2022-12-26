221969ccffce486dc24e229278d6161905e67366
Teuku Muhammad Attar Fadhilah <teuku.muhammad.ft21@mail.umy.ac.id>
2022-12-26 17:18:21 +0700

    Add project files.

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class node
+    {
+        public string info;
+        public node lchild;
+        public node rchild;
+
+        //constructor for the node class
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
